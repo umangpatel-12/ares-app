669490941b314ab30f14a576da311c65b446b94b
Patel Umangkumar <umangkumar170@gmail.com>
2025-03-30 11:22:27 +0530

    Add

---
+++ backend/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify 
+from business import getData
+
+app = Flask(__name__)
+
+@app.route('/')
+def HelloWorld():
+    return "Hello World"
+
+@app.route('/api', methods=['GET'])
+def api():
+    data = getData()
+
+    data = {
+        "data": data
+    }
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=8000,debug=True)
