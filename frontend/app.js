d402516c489d2c7d047ce51bb4ec170981706795
Patel Umangkumar <umangkumar170@gmail.com>
2025-04-01 21:10:17 +0530

    ADD

--- frontend/app.js
+++ frontend/app.js
@@ -1,24 +1,30 @@
 var express = require('express');
 var app = express();
+
 app.set('view engine', 'ejs');
 
-// const URL = process.env.BACKEND_URL || 'http://localhost:8000/api';
-const URL = process.env.BACKEND_URL || 'http://3.7.254.105:8000/api';
+const URL = process.env.BACKEND_URL || 'http://localhost:8000/api' || 'http://13.203.197.13:8000/api' || 'http://13.201.224.97:8000/api';
+// const URL = process.env.BACKEND_URL || 'http://13.201.224.97:8000/api';
+// const URL = process.env.BACKEND_URL || 'http://13.203.197.13:8000/api';
 
 
 const fetch = (...args) =>
     import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 app.get('/', async function (req, res) {
+    const options = {
+        method: 'GET',
+    };
+    fetch(URL, options)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error('error:' + err));
     try {
-        const options = { method: 'GET' };
-        const response = await fetch(URL, options);
-        const data = await response.json();
-
-        console.log(data); // Log response for debugging
-        res.render('index', { data }); // Ensure `data` is passed as an object
+        let response = await fetch(URL, options);
+        response = await response.json();
+        res.render('index', response);
     } catch (error) {
-        console.error('Fetch error:', error);
+        console.log(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
